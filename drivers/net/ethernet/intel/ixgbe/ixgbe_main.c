ffaf343375d0517e25402e1aa70aa3f83bc44c29
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2013-09-19 16:23:49 +0200

    [ixgbe] netmap attach moved to probe
    Conflicts:
    	drivers/net/ethernet/intel/ixgbe/ixgbe_main.c

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -6190,10 +6190,6 @@ int ixgbe_open(struct net_device *netdev)
 	ixgbe_clear_vxlan_port(adapter);
 	udp_tunnel_get_rx_info(netdev);
 
-#ifdef DEV_NETMAP
-	ixgbe_netmap_attach(adapter);
-#endif /* DEV_NETMAP */
-
 	return 0;
 
 err_set_queues:
@@ -9779,6 +9775,10 @@ skip_sriov:
 		hw->mac.ops.setup_link(hw,
 			IXGBE_LINK_SPEED_10GB_FULL | IXGBE_LINK_SPEED_1GB_FULL,
 			true);
+
+#ifdef DEV_NETMAP
+	ixgbe_netmap_attach(adapter);
+#endif /* DEV_NETMAP */
 
 	return 0;
 
