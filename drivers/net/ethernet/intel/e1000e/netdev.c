7f7e1be0ddd5c064cbfd5d95d7286fb774231673
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2013-09-26 12:54:17 +0200

    [e1000e] enable/disable netmap on if up/down

--- drivers/net/ethernet/intel/e1000e/netdev.c
+++ drivers/net/ethernet/intel/e1000e/netdev.c
@@ -3513,6 +3513,10 @@ void e1000e_down(struct e1000_adapter *adapter)
 
 	netif_stop_queue(netdev);
 
+#ifdef DEV_NETMAP
+	netmap_disable_all_rings(netdev);
+#endif
+
 	/* disable transmits in the hardware */
 	tctl = er32(TCTL);
 	tctl &= ~E1000_TCTL_EN;
@@ -3799,6 +3803,10 @@ static int e1000_open(struct net_device *netdev)
 	e1000_irq_enable(adapter);
 
 	netif_start_queue(netdev);
+
+#ifdef DEV_NETMAP
+	netmap_enable_all_rings(netdev);
+#endif /* DEV_NETMAP */
 
 	adapter->idle_check = true;
 	pm_runtime_put(&pdev->dev);
