0773d4f01239086f2025322b333078615f06d596
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2013-10-01 16:15:52 +0200

    [igb] added missing netmap enable

--- drivers/net/igb/igb_main.c
+++ drivers/net/igb/igb_main.c
@@ -2406,6 +2406,10 @@ static int igb_open(struct net_device *netdev)
 
 	netif_tx_start_all_queues(netdev);
 
+#ifdef DEV_NETMAP
+	netmap_enable_all_rings(netdev);
+#endif /* DEV_NETMAP */
+
 	/* start the watchdog. */
 	hw->mac.get_link_status = 1;
 	schedule_work(&adapter->watchdog_task);
