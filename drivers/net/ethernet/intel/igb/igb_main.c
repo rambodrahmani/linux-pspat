e5c38dfba132ce907a175dfc8fa54e7cf8b674d5
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2013-10-01 11:58:55 +0200

    [igb] enable/disable netmap on if up/down

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -1613,6 +1613,10 @@ int igb_up(struct igb_adapter *adapter)
 
 	netif_tx_start_all_queues(adapter->netdev);
 
+#ifdef DEV_NETMAP
+	netmap_enable_all_rings(adapter->netdev);
+#endif /* DEV_NETMAP */
+
 	/* start the watchdog. */
 	hw->mac.get_link_status = 1;
 	schedule_work(&adapter->watchdog_task);
@@ -1645,6 +1649,10 @@ void igb_down(struct igb_adapter *adapter)
 	/* flush both disables and wait for them to finish */
 	wrfl();
 	msleep(10);
+
+#ifdef DEV_NETMAP
+	netmap_disable_all_rings(netdev);
+#endif /* DEV_NETMAP */
 
 	for (i = 0; i < adapter->num_q_vectors; i++)
 		napi_disable(&(adapter->q_vector[i]->napi));
