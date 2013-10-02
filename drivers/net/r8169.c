de87f64a644f90e66398baa46716b703910e7edb
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2013-10-02 14:34:11 +0200

    [r8169] enable/disable netmap rings on if up/down

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -3334,6 +3334,10 @@ static int rtl8169_open(struct net_device *dev)
 
 	napi_enable(&tp->napi);
 
+#ifdef DEV_NETMAP
+	netmap_enable_all_rings(dev);
+#endif /* DEV_NETMAP */
+
 	rtl_hw_start(dev);
 
 	rtl8169_request_timer(dev);
@@ -4168,11 +4172,19 @@ static void rtl8169_wait_for_quiescence(struct net_device *dev)
 	/* Wait for any pending NAPI task to complete */
 	napi_disable(&tp->napi);
 
+#ifdef DEV_NETMAP
+	netmap_disable_all_rings(dev);
+#endif /* DEV_NETMAP */
+
 	rtl8169_irq_mask_and_ack(ioaddr);
 
 	tp->intr_mask = 0xffff;
 	RTL_W16(IntrMask, tp->intr_event);
 	napi_enable(&tp->napi);
+
+#ifdef DEV_NETMAP
+	netmap_enable_all_rings(dev);
+#endif /* DEV_NETMAP */
 }
 
 static void rtl8169_reinit_task(struct work_struct *work)
@@ -4751,7 +4763,12 @@ static void rtl8169_down(struct net_device *dev)
 
 	napi_disable(&tp->napi);
 
+#ifdef DEV_NETMAP
+	netmap_disable_all_rings(dev);
+#endif /* DEV_NETMAP */
+
 core_down:
+
 	spin_lock_irq(&tp->lock);
 
 	rtl8169_asic_down(ioaddr);
