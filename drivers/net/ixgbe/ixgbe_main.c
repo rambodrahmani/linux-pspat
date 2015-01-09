13c61558b80bb474de215055dcd33e9de4bf9ce9
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2015-01-09 17:33:46 +0100

    [ixgbe] detach from netmap on exit

--- drivers/net/ixgbe/ixgbe_main.c
+++ drivers/net/ixgbe/ixgbe_main.c
@@ -7543,6 +7543,10 @@ static void __devexit ixgbe_remove(struct pci_dev *pdev)
 	struct ixgbe_adapter *adapter = pci_get_drvdata(pdev);
 	struct net_device *netdev = adapter->netdev;
 
+#ifdef DEV_NETMAP
+	netmap_detach(netdev);
+#endif /* DEV_NETMAP */
+
 	set_bit(__IXGBE_DOWN, &adapter->state);
 
 	/*
