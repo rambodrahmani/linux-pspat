35b7599e006acd4ed13add9adeb45c3a62c160ab
Vincenzo Maffione <v.maffione@gmail.com>
2015-01-11 15:55:56 +0100

    veth: add netmap support

--- drivers/net/veth.c
+++ drivers/net/veth.c
@@ -39,6 +39,10 @@ struct veth_priv {
 	struct veth_net_stats __percpu *stats;
 };
 
+#if defined(CONFIG_NETMAP) || defined(CONFIG_NETMAP_MODULE)
+#include <veth_netmap.h>
+#endif
+
 /*
  * ethtool interface
  */
@@ -201,6 +205,9 @@ static int veth_open(struct net_device *dev)
 		netif_carrier_on(dev);
 		netif_carrier_on(priv->peer);
 	}
+#ifdef DEV_NETMAP
+	netmap_enable_all_rings(dev);
+#endif /* DEV_NETMAP */
 	return 0;
 }
 
@@ -208,6 +215,9 @@ static int veth_close(struct net_device *dev)
 {
 	struct veth_priv *priv = netdev_priv(dev);
 
+#ifdef DEV_NETMAP
+	netmap_disable_all_rings(dev);
+#endif /* DEV_NETMAP */
 	netif_carrier_off(dev);
 	netif_carrier_off(priv->peer);
 
@@ -238,6 +248,9 @@ static int veth_dev_init(struct net_device *dev)
 
 	priv = netdev_priv(dev);
 	priv->stats = stats;
+#ifdef DEV_NETMAP
+	veth_netmap_attach(dev);
+#endif /* DEV_NETMAP */
 	return 0;
 }
 
@@ -245,6 +258,9 @@ static void veth_dev_free(struct net_device *dev)
 {
 	struct veth_priv *priv;
 
+#ifdef DEV_NETMAP
+	netmap_detach(dev);
+#endif /* DEV_NETMAP */
 	priv = netdev_priv(dev);
 	free_percpu(priv->stats);
 	free_netdev(dev);
