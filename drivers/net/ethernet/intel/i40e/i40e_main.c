329a923e1ff8c83b44a5ce1eb6d66f375a51ea84
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2015-03-03 17:04:28 +0100

    i40e: configure rings
    Conflicts:
    	drivers/net/ethernet/intel/i40e/i40e_main.c

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -2991,6 +2991,10 @@ static int i40e_configure_tx_ring(struct i40e_ring *ring)
 	/* cache tail off for easier writes later */
 	ring->tail = hw->hw_addr + I40E_QTX_TAIL(pf_q);
 
+#ifdef DEV_NETMAP
+	i40e_netmap_configure_tx_ring(ring);
+#endif /* DEV_NETMAP */
+
 	return 0;
 }
 
@@ -3069,6 +3073,11 @@ static int i40e_configure_rx_ring(struct i40e_ring *ring)
 	/* cache tail for quicker writes, and clear the reg before use */
 	ring->tail = hw->hw_addr + I40E_QRX_TAIL(pf_q);
 	writel(0, ring->tail);
+
+#ifdef DEV_NETMAP
+	if (i40e_netmap_configure_rx_ring(ring))
+		return 0;
+#endif /* DEV_NETMAP */
 
 	i40e_alloc_rx_buffers(ring, I40E_DESC_UNUSED(ring));
 
