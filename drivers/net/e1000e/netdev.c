dcd34e431c67c12874638c285fd13813a2a3260d
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2015-09-07 16:31:23 +0200

    removed all enable/disable rings
    Conflicts:
    	drivers/net/igb/igb_main.c

--- drivers/net/e1000e/netdev.c
+++ drivers/net/e1000e/netdev.c
@@ -2856,10 +2856,6 @@ int e1000e_up(struct e1000_adapter *adapter)
 
 	netif_wake_queue(adapter->netdev);
 
-#ifdef DEV_NETMAP
-	netmap_enable_all_rings(adapter->netdev);
-#endif /* DEV_NETMAP */
-
 	/* fire a link change interrupt to start the watchdog */
 	ew32(ICS, E1000_ICS_LSC);
 	return 0;
@@ -2883,10 +2879,6 @@ void e1000e_down(struct e1000_adapter *adapter)
 	/* flush and sleep below */
 
 	netif_stop_queue(netdev);
-
-#ifdef DEV_NETMAP
-	netmap_disable_all_rings(netdev);
-#endif
 
 	/* disable transmits in the hardware */
 	tctl = er32(TCTL);
@@ -3172,10 +3164,6 @@ static int e1000_open(struct net_device *netdev)
 	e1000_irq_enable(adapter);
 
 	netif_start_queue(netdev);
-
-#ifdef DEV_NETMAP
-	netmap_enable_all_rings(netdev);
-#endif /* DEV_NETMAP */
 
 	/* fire a link status change interrupt to start the watchdog */
 	ew32(ICS, E1000_ICS_LSC);
