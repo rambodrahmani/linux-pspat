9c20b861252019ff14819517a0b64a939336e207
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2015-09-07 16:44:11 +0200

    removed all enable/disable rings
    Conflicts:
    	drivers/net/virtio_net.c

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -4198,10 +4198,6 @@ static void ixgbe_up_complete(struct ixgbe_adapter *adapter)
 	/* enable transmits */
 	netif_tx_start_all_queues(adapter->netdev);
 
-#ifdef DEV_NETMAP
-	netmap_enable_all_rings(adapter->netdev);
-#endif
-
 	/* bring the link up in the watchdog, this could race with our first
 	 * link up interrupt but shouldn't be a problem */
 	adapter->flags |= IXGBE_FLAG_NEED_LINK_UPDATE;
@@ -4448,10 +4444,6 @@ void ixgbe_down(struct ixgbe_adapter *adapter)
 	ixgbe_irq_disable(adapter);
 
 	ixgbe_napi_disable_all(adapter);
-
-#ifdef DEV_NETMAP
-	netmap_disable_all_rings(netdev);
-#endif
 
 	adapter->flags2 &= ~(IXGBE_FLAG2_FDIR_REQUIRES_REINIT |
 			     IXGBE_FLAG2_RESET_REQUESTED);
