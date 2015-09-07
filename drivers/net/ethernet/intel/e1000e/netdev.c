beb2c21fd461deee15e259ff4417bef20be82ff3
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2015-09-07 16:44:11 +0200

    removed all enable/disable rings
    Conflicts:
    	drivers/net/ethernet/intel/e1000e/netdev.c
    	drivers/net/ethernet/intel/ixgbe/ixgbe_main.c

--- drivers/net/ethernet/intel/e1000e/netdev.c
+++ drivers/net/ethernet/intel/e1000e/netdev.c
@@ -4236,10 +4236,6 @@ void e1000e_down(struct e1000_adapter *adapter, bool reset)
 
 	netif_stop_queue(netdev);
 
-#ifdef DEV_NETMAP
-	netmap_disable_all_rings(netdev);
-#endif
-
 	/* disable transmits in the hardware */
 	tctl = er32(TCTL);
 	tctl &= ~E1000_TCTL_EN;
@@ -4595,10 +4591,6 @@ static int e1000_open(struct net_device *netdev)
 
 	adapter->tx_hang_recheck = false;
 	netif_start_queue(netdev);
-
-#ifdef DEV_NETMAP
-	netmap_enable_all_rings(netdev);
-#endif /* DEV_NETMAP */
 
 	hw->mac.get_link_status = true;
 	pm_runtime_put(&pdev->dev);
