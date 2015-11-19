e8a9170da24d01f8535d03c4afeb1176cfee005b
Vincenzo Maffione <v.maffione@gmail.com>
2015-11-19 12:46:47 +0100

    virtio-net: perform cleanup after netmap_detach()

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -2660,9 +2660,13 @@ static void remove_vq_common(struct virtnet_info *vi)
 static void virtnet_remove(struct virtio_device *vdev)
 {
 	struct virtnet_info *vi = vdev->priv;
-
 #ifdef DEV_NETMAP
-        netmap_detach(vi->dev);
+	/* Save the pointer, will go away after netmap_detach(). */
+	struct netmap_adapter *token = NA(vi->dev);
+
+	netmap_detach(vi->dev);
+	virtio_netmap_clean_used_rings(vi, token);
+	virtio_netmap_reclaim_unused(vi);
 #endif
 	virtnet_cpu_notif_remove(vi);
 
