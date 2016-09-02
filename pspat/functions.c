f62870b26390ecd040b36e176894619a60cc8230
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2016-09-02 17:19:52 +0200

    pspat: wip on functions

--- pspat/functions.c
+++ pspat/functions.c
@@ -5,18 +5,54 @@
 
 #include "pspat.h"
 
+static int
+pspat_pending_mark(struct pspat_queue *pq)
+{
+	// XXX mark skbs in the local queue
+	return 0;
+}
+
+static void
+pspat_arb_fetch(struct pspat_queue *pq)
+{
+	// XXX copy new skbs from client queue to local queue
+}
+
+static struct sk_buff *
+pspat_arb_get_skb(struct pspat_queue *pq)
+{
+	// XXX get next skb from the local queue
+	return NULL;
+}
+
+#if 0
+static void
+pspat_arb_publish(struct pspat_queue *pq)
+{
+	// XXX copy new skbs to the sender queue
+}
+
+static void
+pspat_arb_ack(struct pspat_queue *pq)
+{
+	// XXX zero out the used skbs in the client queue
+}
+#endif
 
 /* Function implementing the arbiter. */
 int
-pspat_do_arbiter(struct pspat *arb /*, uint64_t now */)
+pspat_do_arbiter(struct pspat *arb)
 {
 	int ndeq = 0;
+	static struct Qdisc *output_queue;
 
-	printk("Arbiter woken up\n");
+	printk(KERN_INFO "Arbiter woken up\n");
 	
-#if 0
 	while (!need_resched()) {
 		int i;
+		uint64_t now = rdtsc();
+
+		rcu_read_lock_bh();
 
 		/*
 		 * bring in pending packets, arrived between next_link_idle and now
@@ -34,24 +70,52 @@ pspat_do_arbiter(struct pspat *arb /*, uint64_t now */)
 				continue;
 			}
 
-			if (now < pq->sch_extract_next) {
+			if (now < pq->arb_extract_next) {
 				continue;
 			}
-			pq->sch_extract_next = now + arb->sched_interval_tsc; // XXX sysctl?
+			pq->arb_extract_next = now + pspat_arb_interval_tsc;
 
-			pspat_arb_fetch(pq); // XXX copy new skbs and zero-out
+			/* 
+			 * copy the new skbs from pq to our local cache.
+			 */
+			pspat_arb_fetch(pq);
 
 			while ( (skb = pspat_arb_get_skb(pq)) ) {
-				struct Qdisc *qdisc = /* obtain from skb (see __dev_queue_xmit) */;
-				rc = qdisc->enqueue(skb, qdisc) & NET_XMIT_MASK;
-				/* enqueue frees the skb by itself in case of error, so we have
-				 * nothing special to do here
+				struct net_device *dev = skb->dev;
+				struct netdev_queue *txq;
+				struct Qdisc *q;
+				int rc;
+
+				/* 
+				 * the client chose the txq before sending
+				 * the skb to us, so we only need to recover it
 				 */
+				txq = netdev_get_tx_queue(dev, 
+						skb_get_queue_mapping(skb));
+
+				q = rcu_dereference_bh(txq->qdisc);
+				rc = q->enqueue(skb, q) & NET_XMIT_MASK;
+				if (unlikely(rc)) {
+					/* enqueue frees the skb by itself
+					 * in case of error, so we have nothing
+					 * to do here
+					 */
+					continue;
+				}
+				if (test_and_set_bit(__QDISC_STATE_SCHED, &q->state)) {
+					/* we have alredy scheduled this Qdisc for
+					 * transmission
+					 */
+					continue;
+				}
+				
+				q->next_sched = output_queue;
+				output_queue = q;
 			}
 		}
-
+#if 0
 		ndeq = 0;
-		while (arb->next_link_idle <= now && ndeq < arb->sched_batch_limit) {
+		while (arb->next_link_idle <= now && ndeq < pspat_arb_batch_limit) {
 			struct sk_buff *skb = qdisc->dequeue(qdisc);
 
 			if (skb == NULL)
@@ -62,14 +126,17 @@ pspat_do_arbiter(struct pspat *arb /*, uint64_t now */)
 		}
 
 		if (ndeq > 0) {
-			for (i = 0; i < arb->queues; i++) {
-				struct pspat_queue *q = arb->queues + i;
-				pspat_arb_publish(q);
+			for (i = 0; i < arb->n_queues; i++) {
+				struct pspat_queue *pq = arb->queues + i;
+				pspat_arb_publish(pq); /* to senders */
+				pspat_arb_ack(pq);     /* to clients */
 			}
 		}
+#endif
+
+		rcu_read_unlock_bh();
 	}
 
-#endif
 	return ndeq;
 }
 
