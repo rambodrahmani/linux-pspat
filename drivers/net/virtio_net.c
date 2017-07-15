f04710899faa96d3938bdc2d9e8c74097f229741
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2017-07-15 15:41:23 +0200

    virtio: remove spurious code

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -1024,17 +1024,6 @@ static int virtnet_receive(struct receive_queue *rq, int budget)
 	void *buf;
 	struct virtnet_stats *stats = this_cpu_ptr(vi->stats);
 
-#ifdef DEV_NETMAP
-        int work_done = 0;
-
-        if (netmap_rx_irq(vi->dev, vq2rxq(rq->vq), &work_done)) {
-		napi_complete(napi);
-		ND("called netmap_rx_irq");
-
-                return 1;
-        }
-#endif
-
 	if (vi->mergeable_rx_bufs) {
 		void *ctx;
 
