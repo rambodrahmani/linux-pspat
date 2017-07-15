7ff30c0cc83421442e2efe6ee42f3d3497a0234a
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2017-07-15 15:36:15 +0200

    virtio: don't mix code and declarations

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -1405,8 +1405,6 @@ static int virtnet_poll(struct napi_struct *napi, int budget)
 	unsigned int received;
 	unsigned int xdp_xmit = 0;
 
-	virtnet_poll_cleantx(rq);
-
 #ifdef DEV_NETMAP
         int work_done = 0;
 	struct virtnet_info *vi = rq->vq->vdev->priv;
@@ -1419,6 +1417,8 @@ static int virtnet_poll(struct napi_struct *napi, int budget)
 		return budget;
 	}
 #endif
+
+	virtnet_poll_cleantx(rq);
 
 	received = virtnet_receive(rq, budget, &xdp_xmit);
 
