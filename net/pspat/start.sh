8b6b9a7c47acabcbefa07ad20f481410232ef046
Vincenzo Maffione <v.maffione@gmail.com>
2017-10-24 16:47:03 +0200

    pspat: improve script

--- net/pspat/start.sh
+++ net/pspat/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 sudo sysctl net.pspat.debug_xmit=0
+sudo sysctl net.pspat.xmit_mode=0
+sudo sysctl net.pspat.tc_bypass=0
+sudo sysctl net.pspat.single_txq=1
 sudo sysctl net.pspat.enable=1
