285471cc36af5806396039dc7f201805f701a821
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2017-11-02 17:35:56 +0100

    ixgbe: call an ixgbe-specific detach function

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -7704,7 +7704,7 @@ static void ixgbe_remove(struct pci_dev *pdev)
 	struct net_device *netdev = adapter->netdev;
 
 #ifdef DEV_NETMAP
-	netmap_detach(netdev);
+	ixgbe_netmap_detach(adapter);
 #endif /* DEV_NETMAP */
 
 #ifdef CONFIG_DEBUG_FS
