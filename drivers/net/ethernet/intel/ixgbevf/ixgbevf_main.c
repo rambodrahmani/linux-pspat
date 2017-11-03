78d0db6c20254a0c0d8215a739e877fb60d0cb74
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2017-11-03 10:06:00 +0100

    ixgebf: call a driver specific version of netmap_detach

--- drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
+++ drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
@@ -3529,7 +3529,7 @@ static void ixgbevf_remove(struct pci_dev *pdev)
 
 
 #ifdef DEV_NETMAP
-	netmap_detach(netdev);
+	ixgbe_netmap_detach(adapter);
 #endif /* DEV_NETMAP */
 
 	set_bit(__IXGBEVF_DOWN, &adapter->state);
