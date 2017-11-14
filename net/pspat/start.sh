96f85092ff1415aa4affb0fbe91dc53841207773
Vincenzo Maffione <v.maffione@gmail.com>
2017-11-14 09:10:41 +0100

    pspat: commit start.sh script

--- net/pspat/start.sh
+++ net/pspat/start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+sudo ethtool -G enp1s0f1 tx 256
+
 sudo sysctl net.pspat.debug_xmit=0
 sudo sysctl net.pspat.xmit_mode=0
 sudo sysctl net.pspat.tc_bypass=0
-sudo sysctl net.pspat.single_txq=1
+sudo sysctl net.pspat.single_txq=0
+sudo sysctl net.pspat.mailbox_entries=512
+sudo sysctl net.pspat.mailbox_line_size=128
+sudo sysctl net.pspat.qdisc_batch_limit=1000
 sudo sysctl net.pspat.enable=1
+
+sudo taskset -pc 7 $(pgrep pspat-arb)
