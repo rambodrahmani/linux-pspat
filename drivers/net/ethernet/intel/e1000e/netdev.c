6ea3ce8b83a62313658a40bfb9f8209507fe2720
Vincenzo Maffione <v.maffione@gmail.com>
2018-02-22 17:58:14 +0100

    e1000e: keep packet-split disabled with netmap

--- drivers/net/ethernet/intel/e1000e/netdev.c
+++ drivers/net/ethernet/intel/e1000e/netdev.c
@@ -3172,6 +3172,10 @@ static void e1000_setup_rctl(struct e1000_adapter *adapter)
 		adapter->rx_ps_pages = pages;
 	else
 		adapter->rx_ps_pages = 0;
+#ifdef DEV_NETMAP
+	/* Keep packet-split disabled with netmap. */
+	adapter->rx_ps_pages = 0;
+#endif /* DEV_NETMAP */
 
 	if (adapter->rx_ps_pages) {
 		u32 psrctl = 0;
