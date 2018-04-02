797c28938c147b5e947fd97bc0513df93a2083c1
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2018-04-02 10:51:57 +0200

    ixgbe: avoid mixing declarations and statements

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -2368,8 +2368,6 @@ static int ixgbe_clean_rx_irq(struct ixgbe_q_vector *q_vector,
 	bool xdp_xmit = false;
 	struct xdp_buff xdp;
 
-	xdp.rxq = &rx_ring->xdp_rxq;
-
 #ifdef DEV_NETMAP
 	/*
 	 * 	 Same as the txeof routine: only wakeup clients on intr.
@@ -2379,6 +2377,8 @@ static int ixgbe_clean_rx_irq(struct ixgbe_q_vector *q_vector,
 	if (nm_irq != NM_IRQ_PASS)
 		return (nm_irq == NM_IRQ_RESCHED) ? budget : 1;
 #endif /* DEV_NETMAP */
+
+	xdp.rxq = &rx_ring->xdp_rxq;
 
 	while (likely(total_rx_packets < budget)) {
 		union ixgbe_adv_rx_desc *rx_desc;
