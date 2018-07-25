9d0d3cee2a2901be57ef48d7723496e773115cd1
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2018-07-25 14:26:40 +0200

    ixgfbe: avoid declaration after statement

--- drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
+++ drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
@@ -1145,8 +1145,6 @@ static int ixgbevf_clean_rx_irq(struct ixgbevf_q_vector *q_vector,
 	bool xdp_xmit = false;
 	struct xdp_buff xdp;
 
-	xdp.rxq = &rx_ring->xdp_rxq;
-
 #ifdef DEV_NETMAP
 	/*
 	 * 	 Same as the txeof routine: only wakeup clients on intr.
@@ -1156,6 +1154,8 @@ static int ixgbevf_clean_rx_irq(struct ixgbevf_q_vector *q_vector,
 	if (nm_irq != NM_IRQ_PASS)
 		return (nm_irq == NM_IRQ_RESCHED) ? budget : 1;
 #endif /* DEV_NETMAP */
+
+	xdp.rxq = &rx_ring->xdp_rxq;
 
 	while (likely(total_rx_packets < budget)) {
 		struct ixgbevf_rx_buffer *rx_buffer;
