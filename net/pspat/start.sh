3a099d7efe8456075e89a56c55b151581875fdff
Rambod Rahmani <rambodrahmani@autistici.org>
2019-12-17 17:21:52 +0100

    Clean up the source code and add comments.

--- net/pspat/start.sh
+++ net/pspat/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Utility bash script to start the PSPAT scheduler.
+
 sudo ethtool -G enp1s0f1 tx 256
 
 sudo sysctl net.pspat.debug_xmit=0
@@ -12,5 +14,7 @@ sudo sysctl net.pspat.arb_qdisc_batch=1000
 sudo sysctl net.pspat.dispatch_batch=1000
 sudo sysctl net.pspat.enable=1
 
+# taskset is used to set the CPU affinity of the pspat running processes.
 sudo taskset -pc 7 $(pgrep pspat-arb)
 sudo taskset -pc 3 $(pgrep pspat-snd)
+
--- net/pspat/stop.sh
+++ net/pspat/stop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Utility bash script to stop the PSPAT scheduler.
+
 sudo sysctl net.pspat.enable=0
+
