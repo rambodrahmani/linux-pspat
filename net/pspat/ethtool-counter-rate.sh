2f063c1504efd05830090f9010c2a6443998a405
Rambod Rahmani <rambodrahmani@autistici.org>
2019-12-17 17:11:24 +0100

    Add comments and clean up the source code.

--- net/pspat/ethtool-counter-rate.sh
+++ net/pspat/ethtool-counter-rate.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+# Utility script to print the number of transmitted packets per second using
+# ethtool.
+
+# default network interface
 ifname="enp1s0f1"
+
+# check if a different net interface was provided
 if [ -n "$1" ]; then
 	ifname="$1"
 fi
 
+# calculate e print the number of packets transmitted per second
 last="0"
 while true; do
 	cur=$(ethtool -S ${ifname} | grep " tx_packets" | cut -d ':' -f 2)
@@ -13,3 +20,4 @@ while true; do
 	last=$cur
 	sleep 1
 done
+
